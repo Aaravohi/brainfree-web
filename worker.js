146f1f89176f5fd6ee848fbd896c4a4e5240618c
Aaravohi <agrahari.aarav11@gmail.com>
2025-10-07 13:22:05 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,54 +1,45 @@
-importScripts('brainfree.js');
+importScripts('brainfree.js'); // keep parser & brainfuck runner functions
 
-let inputQueue = [];
+let pendingInputResolve = null;
+let tape, p, pc, codeStr, jumpStack, jumps;
 
-onmessage = async function(e){
-    const msg = e.data;
+onmessage = async function(e) {
+    if(e.data.type === "run") {
+        codeStr = brainFreeToBF(parseCells(e.data.code));
+        tape = Array(30000).fill(0);
+        p = 0;
+        pc = 0;
+        jumps = {};
+        jumpStack = [];
 
-    if(msg.type === 'run'){
-        const code = msg.code;
-        const bf = brainFreeToBF(parseCells(code));
-        const output = await runBrainfuckAsync(bf);
-        postMessage({type:'output', data: output});
-    } else if(msg.type === 'input'){
-        // receive input from main thread
-        inputQueue.push(msg.data);
+        // Precompute jumps
+        for(let i=0;i<codeStr.length;i++){
+            if(codeStr[i]==='[') jumpStack.push(i);
+            else if(codeStr[i]===']'){ let j=jumpStack.pop(); jumps[i]=j; jumps[j]=i; }
+        }
+
+        let output = "";
+        while(pc < codeStr.length){
+            let ch = codeStr[pc];
+            if(ch==='>') p++;
+            else if(ch==='<') p--;
+            else if(ch==='+') tape[p]=(tape[p]+1)%256;
+            else if(ch==='-') tape[p]=(tape[p]-1+256)%256;
+            else if(ch==='.'){ output+=String.fromCharCode(tape[p]); }
+            else if(ch===','){
+                // pause and request input
+                const inp = await new Promise(resolve => {
+                    pendingInputResolve = resolve;
+                    postMessage({type:"needInput"});
+                });
+                tape[p] = inp.charCodeAt(0);
+            }
+            else if(ch==='[' && tape[p]===0) pc=jumps[pc];
+            else if(ch===']' && tape[p]!==0) pc=jumps[pc];
+            pc++;
+        }
+        postMessage({type:"done", output});
+    } else if(e.data.type === "input") {
+        if(pendingInputResolve) pendingInputResolve(e.data.value);
     }
 };
-
-// Async version of runBrainfuck to wait for input
-function runBrainfuckAsync(code){
-    return new Promise(resolve => {
-        let tape = Array(30000).fill(0), p=0, pc=0, out='';
-        let stack=[], jump={};
-        for(let i=0;i<code.length;i++){
-            if(code[i]==='[') stack.push(i);
-            else if(code[i]===']'){ let j=stack.pop(); jump[i]=j; jump[j]=i; }
-        }
-
-        async function step(){
-            while(pc < code.length){
-                let ch = code[pc];
-                if(ch==='>') p++;
-                else if(ch==='<') p--;
-                else if(ch==='+') tape[p]=(tape[p]+1)%256;
-                else if(ch==='-') tape[p]=(tape[p]-1+256)%256;
-                else if(ch==='.'){ out+=String.fromCharCode(tape[p]); }
-                else if(ch===','){
-                    if(inputQueue.length === 0){
-                        // ask main thread for input
-                        postMessage({type:'needInput'});
-                        return setTimeout(step, 50);
-                    } else {
-                        tape[p] = inputQueue.shift().charCodeAt(0);
-                    }
-                } else if(ch==='[' && tape[p]===0) pc=jump[pc];
-                else if(ch===']' && tape[p]!==0) pc=jump[pc];
-                pc++;
-            }
-            resolve(out);
-        }
-
-        step();
-    });
-}
